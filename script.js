fa92a0fc8a684926ebebd41c781cb6fce190782a
sarahmoe <79814626+s-moe@users.noreply.github.com>
2021-11-26 20:50:59 +0000

    added child object

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ console.log(person)
 person.birthday = 'April 1, 2013'
 
 console.log(person)
+
+const child = {
+  under10: true,
+  hairColor: 'red',
+  favColors: ['purple', 'orange']
+}
+
+console.log(child)
